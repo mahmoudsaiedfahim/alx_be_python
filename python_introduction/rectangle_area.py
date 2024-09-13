761c03b10da47a2eb2019809190f83c28ca12dbd
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-09-13 10:40:28 +0300

    rectangle_area

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+'''Python script that calculates the area of a rectangle.
+   The area of a rectangle is found by multiplying its length by its width.'''
+length = 10
+width = 5
+
+area = length * width
+
+print ("The area of the rectangle is:",area)
