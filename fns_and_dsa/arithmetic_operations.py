219ad166b72822370bb059b79733ef4cdd4f4171
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-09-28 14:09:34 +0300

    arithmetic_operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+'''In this script, define a function that performs 
+basic arithmetic operations. 
+This function, perform_operation, 
+will be imported and used in a separate main.py'''
+
+def perform_operation (num1,num2,operation):
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Error: Division by zero"
+    
+    return result
