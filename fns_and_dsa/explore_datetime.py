dde1f2e9bd95eb97f9be3ec279c5af84146b9c2d
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-09-28 21:23:00 +0300

    explore_datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,14 @@
 '''This script will demonstrate your ability to 
 use the datetime module for handling dates and times 
 in Python.'''
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 def display_current_datetime():
-    return datetime.datetime.now()
+    return datetime.now()
 
 def calculate_future_date(number_of_days):
-    return datetime.datetime.now()+datetime.timedelta(days=number_of_days)
+    return datetime.now()+timedelta(days=number_of_days)
 
 current_date = display_current_datetime()
 print(f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}")
