b8023b9c03956bd1a21a9bb0ccde1fa66525ecbd
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-09-28 21:20:03 +0300

    explore_datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+'''This script will demonstrate your ability to 
+use the datetime module for handling dates and times 
+in Python.'''
+import datetime
+
+def display_current_datetime():
+    return datetime.datetime.now()
+
+def calculate_future_date(number_of_days):
+    return datetime.datetime.now()+datetime.timedelta(days=number_of_days)
+
+current_date = display_current_datetime()
+print(f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}")
+number_of_days=int(input("Enter the number of days to add to the current date: "))
+future_date=calculate_future_date(number_of_days)
+print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
