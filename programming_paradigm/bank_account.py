f73471d4e268a91070b24d41118ed8e0e5b876bd
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-10-05 18:28:43 +0300

    bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,7 +5,7 @@ this class.'''
 class BankAccount:
 
     def __init__(self,account_balance=0):
-        self.account_balance=account_balance
+        self.account_balance=float(account_balance)
 
     def deposit(self,amount):
         self.account_balance += amount
@@ -18,4 +18,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: $[{self.account_balance}]")
+        print(f"Current Balance: ${self.account_balance}")
